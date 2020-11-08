9dc3c49f6360e40f53676fef9370047e925abd8f
Eduardo Lopez <elopez@avascent.com>
2020-11-08 18:00:35 -0500

    finished the js code to add menu items.

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,20 @@
-
-
+// get all class 'container' elements.
+// Loop and add <a> item within <li> item. 
 
 function createLi() {
     const containers = document.getElementsByClassName('container');
-    for(const cont of containers ){
-        console.log(cont.id)
-    }
+    const menuUL = document.getElementById('menu');
+    
+    for(const cont of containers )
+        {
+        
+            let liItem = document.createElement('li');
+            liItem.className = "nav-item";
+            liItem.innerHTML = `<a href="#${cont.id}">${cont.id}</a>`;  
+            menuUL.appendChild(liItem);
+        }
+
 }
+
 
 createLi()

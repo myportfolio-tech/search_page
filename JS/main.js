003f892fe383cf1a4a18845bf3ae8adfb94dc154
Eduardo Lopez <elopez@avascent.com>
2020-11-10 14:40:58 -0500

    got the javascript working properly

--- JS/main.js
+++ JS/main.js
@@ -4,23 +4,31 @@
 
 
 function createLiElements() {
-    const containers = document.getElementsByClassName('container');
-    const menuUL = document.getElementById('main-ul');
     
+    // select all <section> as they all have a class of "section"
+    const containers = document.getElementsByClassName('section');
+    
+    // select the <ul> element already in the menu. it has class of "main-ul"
+    const menuUL = document.getElementById('main-ul');
+
+    // Create and append an <li> item with an <a> element in it.
+    // Append it to the <ul> element
+
     for(const cont of containers )
         {
-        
             let liItem = document.createElement('li');
             liItem.className = "nav-item";
-            liItem.innerHTML = `<div class="menu-container" id="${cont.id}"><a href="#${cont.id}">${cont.id}</a></div>`;  
+            // liItem.innerHTML = `<div class="menu-container" id="${cont.id}"><a href="#${cont.id}">${cont.id}</a></div>`;  
+            
+            //use the container id to create an anchor
+            liItem.innerHTML = `<a href="#${cont.id}">${cont.id}</a>`;  
             menuUL.appendChild(liItem);
         }
-
 }
-
 
 createLiElements()
 
+// Determine if a <container> is in the main display range 
 
 function isOnTop(yprop) 
     {
@@ -29,30 +37,55 @@ function isOnTop(yprop)
             return true;
         }
         else
+        {
+            return false;
+        }
+    };
+
+
+function getMenuLiElement(matchedID)
+    {
+        //get all <li> under the <ul> element
+        const menuListItems = document.getElementsByClassName("nav-item");             
+        
+        //get the text in the <a> element
+        for(const item of menuListItems)
             {
-                return false
+                if (matchedID == item.firstChild.innerHTML)
+                    {
+                        console.log(item.firstChild.innerHTML);
+                        item.firstChild.classList.add("active-menu");
+                    }
+                else
+                    {
+                        item.firstChild.classList.remove("active-menu");
+                    } 
+
             }
-    } 
+    }
+
+
 
 
 // This function is used to test the position of the containers
 // We can use it then to troubleshoot when to set a container as active
+
 function displayContainerInfo()
     {
-    const containers = document.getElementsByClassName('container');
+    const containers = document.getElementsByClassName('section');
     console.clear();
     for(let cont of containers)
 
         {
             const props = cont.getBoundingClientRect();
-            console.log(cont.id)
-            console.log(props.y);
-            console.log(`${cont.id} is on top: ${isOnTop(props.y)}`);
             if (isOnTop(props.y)) 
                 {
-                    const elem = document.getElementById(cont.id)
-                    console.log(elem)
-                    elem.classList.add("active")
+                    const elem = document.getElementById(cont.id);
+                    console.log(elem);
+                    elem.classList.add("active");
+                    getMenuLiElement(cont.id);
+
+
                 }
             else
                 {
@@ -70,12 +103,5 @@ document.addEventListener('scroll', function() {
 });
 
 
+// CALL FUNCTIONS //
 
-
-const navvItems = document.getElementById('main-ul');
-
-
-for(const item of navvItems.children) 
-    {
-        console.log(item.getElementsByTagName('a')[0])
-    }
